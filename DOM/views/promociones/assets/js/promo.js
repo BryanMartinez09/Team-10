9fc1194f664c220dec008523e803b502e0f459ee
Alejo <alejo7d1@gmail.com>
2025-07-19 21:42:45 -0600

    Cambios promociones: correccion menor y anuncio agregado

--- DOM/views/promociones/assets/js/promo.js
+++ DOM/views/promociones/assets/js/promo.js
@@ -1,9 +1,10 @@
+//Funcion para resaltar el café del día
 window.onload = function() {
   const hoy = new Date().getDay(); // 0 = Domingo, 1 = Lunes, ..., 6 = Sábado
   const cafes = document.querySelectorAll(".cafe");
 
   cafes.forEach((cafe, index) => {
-    if(index === (hoy === 0 ? 6 : hoy - 1)) { // Para poner lunes como 0
+    if(index === (hoy === 0 ? 6 : hoy - 1)) { 
       cafe.classList.add("cafe-del-dia");
     }
   });
